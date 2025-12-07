6942da0ccfbe7efcd4060d9061415173bf138a4f
Daksh-Official <daksh.official9705@gmail.com>
2025-12-08 02:23:14 +0530

    added anotations

--- src/main/java/com/hrservices/hrservicesbackend/HrservicesbackendApplication.java
+++ src/main/java/com/hrservices/hrservicesbackend/HrservicesbackendApplication.java
@@ -2,8 +2,12 @@ package com.hrservices.hrservicesbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.hrservices.hrservicesbackend.Entity")
+@EnableJpaRepositories("com.hrservices.hrservicesbackend.Repositories")
 public class HrservicesbackendApplication {
 
 	public static void main(String[] args) {
