3fb93c39b85e21685a636101777e7ccf586d4235
Daksh-Official <daksh.official9705@gmail.com>
2025-12-08 02:16:09 +0530

    added getter and setter

--- src/main/java/com/hrservices/hrservicesbackend/Config/SecurityConfig.java
+++ src/main/java/com/hrservices/hrservicesbackend/Config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(e -> e.authenticationEntryPoint((AuthenticationEntryPoint) entryPoint))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("https://hr-services-ochre.vercel.app/").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
